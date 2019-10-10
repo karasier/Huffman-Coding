fb2358c3cd9113732e86661a5d9f74ef4d4b6a4c
karasi <40154200+karasier@users.noreply.github.com>
2019-10-10 18:58:25 +0900

    Add Huffman.rb & some changes
    ・Ruby版を追加
    ・Python版を微修正

--- Huffman.py
+++ Huffman.py
@@ -3,19 +3,7 @@ import re
 import collections
 import math
 
-# 入力文字列の条件を表示するメソッド
-def output_conditions():
-    print("<<入力する文字列は次の条件を満たしてください。>>")
-    print("1.使用する文字が小文字のアルファベットのみ")
-    print("2.文字列の長さが40文字以下")
-    print("3.文字の種類が10種類以下\n")
-
-# 生起確率を計算するメソッド
-def compute_occurrence_probability(number_of_chars,collections_array):
-  occurrence_probability_array = [ [collections_array[i][0],collections_array[i][1]/number_of_chars] for i in range(len(collections_array))]
-  return occurrence_probability_array
-
-# ハフマン木の葉を示すクラス
+# ハフマン木の葉と節点を示すクラス
 class Node:
   # コンストラクタ
   def __init__(self,char = None,occurrence_probability = None,left = None,right = None):
@@ -27,48 +15,54 @@ class Node:
 # ハフマン木を示すクラス
 class HuffmanTree:
   # コンストラクタ
-  def __init__(self,input_string,string_array,string_collections,collections_array,occurrence_probability_array):
+  def __init__(self,input_string,collections_array):
     self.encode_dict = {}
     self.input_string = input_string
-    self.string_array = string_array
-    self.string_collections = string_collections
     self.collections_array = collections_array
-    self.occurrence_probability_array = occurrence_probability_array
+
+  # 出現確率を計算するメソッド
+  def compute_occurrence_probability(self):
+    self.occurrence_probability_array = [ self.collections_array[i][1]/len(self.input_string) for i in range(len(self.collections_array)) ]
 
   # 符号化するメソッド
   def encode(self):
     nodes = [] # 探索していない葉を格納する配列
+    temp = []
+
+    # 出現確率の計算
+    self.compute_occurrence_probability()
 
     # 葉の生成
     for i in range(len(self.collections_array)):
-      nodes.append(Node(self.occurrence_probability_array[i][0],self.occurrence_probability_array[i][1]))
+      nodes.append(Node(self.collections_array[i][0],self.occurrence_probability_array[i]))
 
+    # 文字数が1の場合0を割り当てて終了
     if len(self.collections_array) == 1:
       self.encode_dict[nodes[0].char] = "0"
       return self.encode_dict
 
-    temp = []
-
-    # 探索していない葉が1つになるまで繰り返す
+    # 葉の数が1になるまで繰り返し
     while len(nodes) > 1:
-      # 生起確率の最も小さい2つの葉の生起確率を合計して新しい葉を生成
+      # 出現確率の小さい方から2つ取り出して、節点を生成
       for i in range(2):
         element = nodes[-1]
         temp.append(element)
         nodes.remove(element)
 
       # 新しい葉を生成
-      new_node = Node(char=None, occurrence_probability=temp[0].occurrence_probability+temp[1].occurrence_probability, right=temp[0], left=temp[1])
-      temp = []
+      new_node = Node(char=None, occurrence_probability=temp[0].occurrence_probability + temp[1].occurrence_probability,left=temp[1],right=temp[0])
       # 探索していない葉の配列に格納
       nodes.append(new_node)
+      temp = []
 
     # 符号語の割り当て
     self.allocate_codewords(nodes[0],"")
-    return self.encode_dict
+    
+    # 符号語部分だけ取り出してreturn
+    return list(map(lambda x: x[1], self.encode_dict.items()))
+
 
   # 符号語を割り当てるメソッド
-  # 再帰構造
   def allocate_codewords(self,node,code):
 
     # nodeがクラスNodeのオブジェクトでないときreturn
@@ -86,12 +80,14 @@ class HuffmanTree:
 
 if __name__ == "__main__":
   print("[ハフマン符号の構成プログラム]\n")
-  output_conditions()
+  print("<<入力する文字列は次の条件を満たしてください。>>")
+  print("1.使用する文字が小文字のアルファベットのみ")
+  print("2.文字列の長さが40文字以下")
+  print("3.文字の種類が10種類以下\n")
+
   input_string = input("符号化したい文字列を入力\n>>")
   pattern = re.compile("[a-z]+") # 小文字のアルファベットの正規表現パターンを作成
-  string_array = list(input_string) # 入力文字列を配列に変換
-  string_collections = collections.Counter(string_array) # 文字の種類と出現回数を保持するオブジェクト
-  collections_array = string_collections.most_common() # 文字の種類と出現回数を出現回数順に並べられた配列に変換
+  string_collections = collections.Counter(list(input_string)) # 文字の種類と出現回数を保持するオブジェクト
 
   # 入力文字列がパターンにマッチしない
   # 入力文字列の長さが40文字より大きい
@@ -106,18 +102,15 @@ if __name__ == "__main__":
       print("[文字の種類が10種類を超えています。]\n")
 
     input_string = input("再入力\n>>")
-    string_array = list(input_string)
-    string_collections = collections.Counter(string_array)
-    collections_array = string_collections.most_common()
+    string_collections = collections.Counter(list(input_string))
 
-  occurrence_probability_array = compute_occurrence_probability(len(input_string),collections_array) # 生起確率と文字の組となる2次元配列を作成
-  Huffman_Tree = HuffmanTree(input_string,string_array,string_collections,collections_array,occurrence_probability_array) # ハフマン木をインスタンス生成
-  codewords = Huffman_Tree.encode() # 入力文字列を符号化
-  codewords_array = list(codewords.items()) # 文字と符号語の組の辞書型を配列に変換
-  result = [ [collections_array[i][0],collections_array[i][1],occurrence_probability_array[i][1],codewords_array[i][1]] for i in range(len(collections_array))] # 結果をまとめる
+  collections_array = string_collections.most_common() # 文字の種類と出現回数を出現回数順に並べられた配列に変換
 
-  entropy = -sum([ result[i][2] * math.log2(result[i][2]) for i in range (len(collections_array))]) # エントロピーの計算
-  average_code_length = sum([ result[i][2] * len(result[i][3]) for i in range (len(collections_array))]) # 平均符号長の計算
+  tree = HuffmanTree(input_string,collections_array) # ハフマン木をインスタンス生成
+  codewords = tree.encode() # 入力文字列を符号化
+
+  entropy = -sum([ tree.occurrence_probability_array[i] * math.log2(tree.occurrence_probability_array[i]) for i in range (len(collections_array))]) # エントロピーの計算
+  average_code_length = sum([ tree.occurrence_probability_array[i] * len(codewords[i]) for i in range (len(collections_array))]) # 平均符号長の計算
   efficiency = entropy/average_code_length # 効率の計算
 
   print("\n----------------------------------------------------")
@@ -126,8 +119,8 @@ if __name__ == "__main__":
   print("[結果]")
   print("| 文字 | 文字数 | 出現確率 | ハフマン符号")
   # 結果の出力
-  for output in result:
-    print("|  ",output[0]," |   ",output[1],"  | ","{:.5f}".format(output[2]),"|",output[3])
+  for i in range(len(collections_array)):
+    print("|  ",collections_array[i][0]," |   ",collections_array[i][1],"  | ","{:.5f}".format(tree.occurrence_probability_array[i]),"|",codewords[i])
 
   print("\nエントロピー H(A) :",entropy)
   print("平均符号長 L : ",average_code_length)
---
+++ Huffman.rb
@@ -0,0 +1,160 @@
+# ハフマン木の葉と節点を示すクラス
+class Node
+  def initialize(char = nil,occurrence_probability = nil,left = nil,right = nil)
+    @char = char # 文字
+    @occurrence_probability = occurrence_probability # 出現確率
+    @left = left
+    @right = right
+  end
+
+  attr_accessor :char,:occurrence_probability,:left,:right
+end
+
+# ハフマン木を示すクラス
+class HuffmanTree
+  def initialize(input_string,string_array)
+    @encode_dict = Hash.new("") # 符号語を保持するハッシュ
+    @input_string = input_string # 入力された文字列
+    @string_array = string_array # 文字と出現回数を保持する配列
+  end
+
+  # 出現確率を計算するメソッド
+  def compute_occurrence_probability
+    @occurrence_probability_array = @string_array.map{ |item,times| times.to_f/@input_string.size.to_f}
+  end
+
+  # 符号化するメソッド
+  def encode
+    nodes = []
+    temp = []
+
+    # 出現確率を計算
+    compute_occurrence_probability
+
+    # 葉の生成
+    @string_array.size.times do |i|
+      nodes.push(Node.new(@string_array[i][0],@occurrence_probability_array[i]))
+    end
+
+    # 葉の数が1の場合0を割り当てて終了
+    if nodes.size == 1 then
+      @encode_dict[nodes[0].char] = "0"
+      return @encode_dict
+    end
+
+    # 葉の数が1になるまで繰り返し
+    while nodes.size > 1 do
+      # 出現確率の小さい方から2つ取り出して、節点を生成
+      2.times do |i|
+        element = nodes[-1]
+        temp.push(element)
+        nodes.delete(element)
+      end
+
+      # 節点の生成
+      new_node = Node.new(nil,temp[0].occurrence_probability + temp[1].occurrence_probability,temp[1],temp[0])
+      nodes.append(new_node)
+      temp = []
+    end
+
+    # 符号語の割り当て
+    allocate_codewords(nodes[0],"")
+
+    return @encode_dict.to_a.transpose[1]
+  end
+
+  # 符号語を割り当てるメソッド
+  def allocate_codewords(node,codeword)
+
+    # nodeがクラスNodeのオブジェクトでないときreturn
+    unless node.instance_of?(Node) then
+      return
+    end
+
+    # nodeが葉の場合(charがnilでない場合)符号語を割り当ててreturn
+    if node.char then
+      @encode_dict[node.char] = codeword
+      return
+    end
+
+    # 再帰呼び出し
+    allocate_codewords(node.left,codeword+"0")
+    allocate_codewords(node.right,codeword+"1")
+  end
+
+  attr_reader :occurrence_probability_array
+end
+
+# 文字の出現回数をカウントする関数
+def count_element(input_string)
+  hash = Hash.new(0) # ハッシュの初期値を0に設定
+  input_string.split("").each do |item|
+    hash[item] += 1 # 文字の出現回数を記録
+  end
+  return hash
+end
+
+if __FILE__ == $0
+  puts "[ハフマン符号の構成プログラム]"
+  puts "\n<<入力する文字列は次の条件を満たしてください。>>"
+  puts "1.使用する文字が小文字のアルファベットのみ"
+  puts "2.文字列の長さが40文字以下"
+  puts "3.文字の種類が10種類以下"
+
+  input_string = ""
+
+  while input_string == "" do
+    print "\n符号化したい文字列を入力\n>>"
+    input_string = gets.chomp
+  end
+
+  string_dict = count_element(input_string) # 文字の出現回数をカウント
+  string_array = string_dict.sort_by{|key,value| [-value,key]} # 文字の出現回数を降順で並び替えて配列に変換
+
+  # 入力文字列がパターンにマッチしない
+  # 入力文字列の長さが40文字より大きい
+  # 文字の種類が10より大きい
+  # 上記のいずれかの条件を満たしたら再入力
+  while input_string =~ /[^a-z]+/ or input_string.size > 40 or string_array.size > 10 do
+    if input_string =~ /[^a-z]+/ then
+      puts "[入力可能文字はアルファベットの小文字のみです。]"
+    elsif input_string.size > 40 then
+      puts "[文字数が40文字を超えています。]"
+    else
+      puts "[文字の種類が10種類を超えています。]"
+    end
+
+    print "\n再入力\n>>"
+    input_string = gets.chomp
+    string_dict = count_element(input_string) # 文字の出現回数をカウント
+    string_array = string_dict.sort_by{|key,value| [-value,key]} # 文字の出現回数を降順で並び替えて配列に変換
+  end
+
+  # ハフマン木のインスタンス生成
+  tree = HuffmanTree.new(input_string,string_array)
+
+  # 符号化
+  codewords = tree.encode()
+  # エントロピー、平均符号長、効率の計算
+  entropy = tree.occurrence_probability_array.inject(0){ |sum,occurrence_probability| sum + -(occurrence_probability * Math.log2(occurrence_probability)) }
+  average_code_length = 0.0
+  tree.occurrence_probability_array.each.with_index{ |occurrence_probability,i| average_code_length += occurrence_probability * codewords[i].size}
+  efficiency = entropy/average_code_length
+
+  # 結果の出力
+  puts "\n----------------------------------------------------"
+  puts "[入力した文字列]"
+  puts input_string
+  puts "\n[結果]"
+  puts "| 文字 | 文字数 |  出現確率  | ハフマン符号"
+  # 結果の出力
+
+  string_array.size.times do |i|
+    puts "|  #{string_array[i][0]}   |    #{string_array[i][1]}   |  #{sprintf("%.6f",tree.occurrence_probability_array[i])}  | #{codewords[i]}"
+  end
+
+  puts "\nエントロピー H(A) : #{entropy}"
+  puts "平均符号長 L : #{average_code_length}"
+  puts "効率 η : #{efficiency}"
+  puts "----------------------------------------------------"
+end
